5540e7cdb21fe39309e93a021d5556a8e76b65f8
nitin katiyar <nkatiyar11@gmail.com>
2023-06-14 21:05:24 +0530

    anagram code done

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,33 @@
 */
 
 function isAnagram(str1, str2) {
+  const lowerStr1 = str1.toLowerCase();
+  const lowerStr2 = str2.toLowerCase();
 
+  const charMap1 = {};
+  const charMap2 = {};
+
+  for (let char of lowerStr1) {
+    charMap1[char] = (charMap1[char] || 0) + 1;
+  }
+
+  for (let char of lowerStr2) {
+    charMap2[char] = (charMap2[char] || 0) + 1;
+  }
+
+  for (let char in charMap1) {
+    if (charMap1[char] !== charMap2[char]) {
+      return false;
+    }
+  }
+
+  for (let char in charMap2) {
+    if (charMap1[char] === undefined) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 module.exports = isAnagram;
